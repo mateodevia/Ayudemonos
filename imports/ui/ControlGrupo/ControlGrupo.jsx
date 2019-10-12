fec15705dbf2ea76beeea56843975e53cfe2afdb
mateo <m.devia@uniandes.edu.co>
2019-10-12 14:16:52 -0500

    return to previous state

--- imports/ui/ControlGrupo/ControlGrupo.jsx
+++ imports/ui/ControlGrupo/ControlGrupo.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./ControlGrupo.css";
 
-function ControlGrupo() {
-  const [modalShow, setModalShow] = React.useState(false);
-  const [modalShow2, setModalShow2] = React.useState(false);
-  return (
-    <React.Fragment>
-      <button className="botonControl" onClick={() => setModalShow(true)}>
-        Agregar Integrante
-      </button>
-      <button className="botonControl" onClick={() => setModalShow2(true)}>
-        Agregar Tarea
-      </button>
-    </React.Fragment>
-  );
+class ControlGroup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <button className="botonControl">Agregar Integrate</button>
+        <button className="botonControl">Agregar Tera</button>
+      </React.Fragment>
+    );
+  }
 }
 
-export default ControlGrupo;
+export default ControlGroup;
