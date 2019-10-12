f68efab5d2229e344189dd82507b674056299202
mateo <m.devia@uniandes.edu.co>
2019-10-12 12:57:13 -0500

    modals working but missing content in both

--- imports/ui/ControlGrupo/ControlGrupo.jsx
+++ imports/ui/ControlGrupo/ControlGrupo.jsx
@@ -1,19 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import ModalAgregarIntegrante from "../ModalAgregarIntegrante/ModalAgregarIntegrante";
+import ModalAgregarTarea from "../ModalAgregarTarea/ModalAgregarTarea";
 import "./ControlGrupo.css";
 
-class ControlGrupo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <button className="botonControl">Agregar Integrante</button>
-        <button className="botonControl">Agregar Tarea</button>
-      </React.Fragment>
-    );
-  }
+function ControlGrupo() {
+  const [modalShow, setModalShow] = React.useState(false);
+  const [modalShow2, setModalShow2] = React.useState(false);
+  return (
+    <React.Fragment>
+      <button className="botonControl" onClick={() => setModalShow(true)}>
+        Agregar Integrante
+      </button>
+      <button className="botonControl" onClick={() => setModalShow2(true)}>
+        Agregar Tarea
+      </button>
+      <ModalAgregarIntegrante
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+      <ModalAgregarTarea
+        show={modalShow2}
+        onHide={() => setModalShow2(false)}
+      />
+    </React.Fragment>
+  );
 }
 
 export default ControlGrupo;
---
+++ imports/ui/ModalAgregarIntegrante/ModalAgregarIntegrante.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+function ModalAgregarIntegrante(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Agregar Integrante al Grupo
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <button className="boton" onClick={props.onHide}>
+          Close
+        </button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default ModalAgregarIntegrante;
